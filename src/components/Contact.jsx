1bed9d2f5afafcd6cfedcc52ad1652a48d7b382f
iamkaizen <kimsokhom654@gmail.com>
2025-04-08 20:05:52 +0700

    Completed: Website Footer

---
+++ src/components/Contact.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "ce1c0917-a9db-41df-b175-5644c98f5a22");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      setResult("");
+    }
+  };
+
+  return (
+    <div
+      className="container text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h2 className="text-2xl sm:text-4xl font-bold mb-2">
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 font-light">
+          With Us
+        </span>
+      </h2>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Ready to Make a Move? Let's Build Your Future Together
+      </p>
+
+      {/* contact form */}
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded px-4 py-3 mt-2"
+              type="text"
+              name="Name"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded px-4 py-3 mt-2"
+              type="email"
+              name="Email"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+          <div className="w-full my-6 text-left">
+            Message
+            <textarea
+              className="w-full border border-gray-300 rounded px-4 py-3 mt-2 h-48 resize-none"
+              name="Message"
+              placeholder="Message"
+              required
+            ></textarea>
+          </div>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded hover:bg-blue-500 transition duration-300 cursor-pointer">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+  useEffect(() => {
+    const updateCardsToShow = () => {
+      if (window.innerWidth >= 1024) {
+        setCardsToShow(projectsData.length);
+      } else {
+        setCardsToShow(1);
+      }
+    };
+    updateCardsToShow();
+    window.addEventListener("resize", updateCardsToShow);
+    return () => {
+      window.removeEventListener("resize", updateCardsToShow);
+    };
+  }, []);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div
       id="Projects"
@@ -15,6 +43,54 @@ const Projects = () => {
       <p className="text-center text-gray-500 max-w-80 mb-8 mx-auto">
         Crafting Spaces, Building Legacies—Explore Our Portfolio
       </p>
+
+      {/* slider button */}
+      <div className="flex justify-end items-center mb-8">
+        <button
+          onClick={prevProject}
+          className="p-3 bg-gray-200 rounded mr-2 hover:bg-gray-300 hover:shadow-md transition duration-300"
+          aria-label="Previous Project"
+        >
+          <img src={assets.left_arrow} alt="Preview Button" />
+        </button>
+        <button
+          onClick={nextProject}
+          className="p-3 bg-gray-200 rounded mr-2 hover:bg-gray-300 hover:shadow-md transition duration-300"
+          aria-label="Next Project"
+        >
+          <img src={assets.right_arrow} alt="Next Button" />
+        </button>
+      </div>
+
+      {/* project slider container */}
+      <div className="overflow-hidden">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${currentIndex * (100 / cardsToShow)}%)`,
+          }}
+        >
+          {projectsData.map((project, index) => (
+            <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span className="px-1">|</span> {project.price}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
